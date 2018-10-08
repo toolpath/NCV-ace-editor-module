5d8bb6f7a410ee5a3ceedb54a9a5315dd0dfcd0a
Xander Luciano <vipercodegames@gmail.com>
2018-10-07 18:03:32 -0700

    feat: Initial Version (#1)

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,11 +1,39 @@
 <template>
-<div>
-    Works!
+<div style=" width: 80%; margin: auto;">
+    <div>Plugin Module Works!</div>
+    <div id="ace" style="height: 200px; width: 100%; border: solid 1px rgba(0,0,0,.3);"></div>
 </div>
 </template>
 
 <script>
-export default {
+    export default {
+        mounted() {
+            const editor = ace.edit('ace');
 
-}
+            editor.setOptions({
+                theme: 'ace/theme/chrome',
+                mode: 'ace/mode/javascript',
+                printMargin: false,
+                vScrollBarAlwaysVisible: true,
+                scrollPastEnd: 1,
+                fontSize: 12,
+            });
+
+            editor.setValue(
+`    export default {
+        mounted() {
+            const editor = ace.edit('ace');
+            editor.setOptions({
+                theme: 'ace/theme/chrome',
+                mode: 'ace/mode/javascript',
+                printMargin: false,
+                vScrollBarAlwaysVisible: true,
+                scrollPastEnd: 1,
+                fontSize: 12,
+            });
+            editor.setValue(\`\`, -1);
+        }
+    }`, -1);
+        }
+    }
 </script>
